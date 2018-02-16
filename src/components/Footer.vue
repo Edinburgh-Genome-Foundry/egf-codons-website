4aecc35331481413cc9cd9fa24ee7baf55a34605
Zulko <valentin.zulkower@gmail.com>
2018-02-16 17:50:24 +0000

    png version of horizontal logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 footer
   .container
   p.center
-    img(src='/static/imgs/logos/egf-codon-horizontal.svg')
+    img(src='/static/imgs/logos/egf-codon-horizontal.png' width=190)
     br
     | © Edinburgh Genome Foundry, 2018 <br/>
     | The EGF is supported by the Research Councils UK and University of Edinburgh
@@ -17,8 +17,12 @@ footer {
   margin-bottom: 1em;
   padding-bottom: 1em;
   color: #999999;
-  filter: grayscale(100%);
   text-align: center;
+}
+
+img {
+  filter: grayscale(100%);
+  opacity: 0.6;
 }
 
 </style>
